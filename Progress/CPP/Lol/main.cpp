237c4e0c0d8ed514bb2f949f3160eba0875a83e4
Arvind Sagar <48724777+cryoelite@users.noreply.github.com>
2020-06-13 23:52:18 +0530

    Day51
    Lambdas

--- Progress/CPP/Lol/main.cpp
+++ Progress/CPP/Lol/main.cpp
@@ -1,30 +1,52 @@
 #include<iostream>
-#include<string.h>
+#include<array>
 
-template<typename Y>
-using PPAP = Y;
 
-void add_stuff()
-{
-	std::cout << "The End" << std::endl;
-}
 
-template<typename...T, typename X>
-void add_stuff(const X& first, const T&... args)
-{
-	std::cout << first ;
-	if (sizeof...(args) != 0)
-		std::cout << " , ";
-	else
-		std::cout << std::endl;
-	add_stuff(args...);
-}
+template<typename T>
+using Comp = std::pair<int, int>(*)(T x, T y);
+//using Comp = bool(*)(int, int);
+//template<typename T, typename X>
+//struct Ad
+//{
+//
+//	T operator ()(T first, T second, X third)
+//	{
+//		std::cout << std::boolalpha << third(first, second) << std::endl;
+//		return first + second;
+//	}
+//};
 
 int main()
 {
-	add_stuff(1, "2", 2.3, "hello", 4.0f);
-	PPAP<int> p{ 2 };
-	PPAP<float> s{ 2 };
-	std::cout << typeid(s).name() << std::endl;
+	/*Ad<int, Comp> a;
+	auto c{ [](auto a, auto b) {
+		return a < b;
+} };
+	std::cout << a(2, 3, [](auto a, auto b)->bool {
+		return a < b;
+		}) << std::endl;
+	std::cout << a(2, 3, c);*/
+
+	std::array<int, 6> a{ 1,2,5,3,2,4 };
+	auto x = [](auto b, auto c)->std::pair<int, int> {
+		std::pair<int, int> a{*b,*b};
+		while (b != c)
+		{
+			if (a.first > * b)
+			{
+				a.first = *b;
+			}
+			if (a.second < *b)
+			{
+				a.second = *b;
+			}
+			++b;
+		}
+		return a;
+	};
+	std::pair<int, int> b{ x(a.begin(),a.end()) };
+	std::cout << b.first << ":" << b.second<<std::endl;
 	return 0;
+
 }
