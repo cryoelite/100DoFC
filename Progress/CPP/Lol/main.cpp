2876889483670d898d9a56bf4ed8d6543935d967
Arvind Sagar <48724777+cryoelite@users.noreply.github.com>
2020-07-04 04:25:17 +0530

    std accumulate

--- Progress/CPP/Lol/main.cpp
+++ Progress/CPP/Lol/main.cpp
@@ -1,56 +1,14 @@
-#include <iostream>
+#include<iostream>
 #include<vector>
+#include<numeric>
+#include<functional>
 
-void sortIt(std::vector<int> &a)
+
+
+int main()
 {
-	int temp{0};
-	while (temp == 0)
-	{
-		int tempS{ 0 };
-		for (int i{ 0 }; i < a.size()-1; ++i)
-		{
-			if (a[i] > a[i + 1])
-			{
-				tempS = 1;
-				int tempX{ a[i] };
-				a[i] = a[i + 1];
-				a[i + 1] = tempX;
-			}
-			
-		}
-		if (tempS != 0) {
-			temp = 0;
-		}
-		else
-		{
-			temp = 1;
-		}
-	}
-
-
-	for (auto& x : a)
-	{
-		std::cout << x << std::endl;
-	}
-	return;
-}
-
-int main() {
-	int size;
-	std::cout << "Enter number of elements: ";
-	std::cin >> size;
-	std::cout << "\nEnter element values: ";
-	std::vector<int> a;
-	for (int i = 0; i < size; ++i)
-	{
-		int temp;
-		std::cin >> temp;
-		a.push_back(temp);
-		std::cout << std::endl;
-	}
-	sortIt(a);
-
+	std::vector<int> a{ 1,2,3,4 };
+	std::cout<<std::accumulate(a.begin(), a.end(), 0,std::plus<int>());
 
 	return 0;
-
 }
