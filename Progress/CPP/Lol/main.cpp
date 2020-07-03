25e3b4ef7633afc993b5f13a5a11cb4be632d24a
Arvind Sagar <48724777+cryoelite@users.noreply.github.com>
2020-07-04 05:05:15 +0530

    constexpr and folds

--- Progress/CPP/Lol/main.cpp
+++ Progress/CPP/Lol/main.cpp
@@ -1,14 +1,59 @@
 #include<iostream>
-#include<vector>
-#include<numeric>
-#include<functional>
+#include<type_traits>
 
 
+
+int sum()
+{
+	return 0;
+}
+
+template<typename T, typename ...ArgX>
+int sum(T a, ArgX ...args)
+{
+	return a + sum(args...);
+}
+
+
+template<typename ...ArgX>
+int sum2(ArgX ...args)
+{
+	return (args + ... + 0);
+}
+
+
+template<typename ...ArgX, typename T>
+int sum3(T a, ArgX ...args)
+{
+	return (a(args)+ ... + 0);
+}
+
+template<typename T>
+void whatIsIt(T &val)
+{
+	if constexpr(std::is_pointer_v<T>) {
+		std::cout << "Pointer";
+	} 
+	else if constexpr(std::is_array_v<T>)
+	{
+		std::cout << "Array";
+	}
+	else
+	{
+		std::cout << "Dunno Dun Care";
+	}
+}
 
 int main()
-{
-	std::vector<int> a{ 1,2,3,4 };
-	std::cout<<std::accumulate(a.begin(), a.end(), 0,std::plus<int>());
+{/*
+	std::cout << sum(1, 2, 3, 4);
+	std::cout << sum2(1, 2, 3, 4);*/
+	//std::cout << sum3([](int x) {
+	//	return x; },1,2,3,4);
+	int* p = new int{1};
+	int a[]={0,1};
+	whatIsIt(p);
+	whatIsIt(a);
 
 	return 0;
 }
