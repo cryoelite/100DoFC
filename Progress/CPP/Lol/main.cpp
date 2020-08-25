09f5bbbed2ad28dc3d7371048b82ef932784335d
Arvind Sagar <48724777+cryoelite@users.noreply.github.com>
2020-08-25 23:12:28 +0530

    ye

--- Progress/CPP/Lol/main.cpp
+++ Progress/CPP/Lol/main.cpp
@@ -1,56 +1,17 @@
 #include<iostream>
-#include<set>
-#include<string>
-#include<sstream>
+#include<array>
 
-
-void filler(std::multiset<int>& tempVec, std::string& tempString)
+using namespace std;
+template<typename T,int size>
+void SIZE(T arr[size])
 {
-	std::stringstream splitter(tempString);
-	std::string splitVal{};
-	while (std::getline(splitter, splitVal, ' '))
-	{
-		tempVec.insert(stoi(splitVal));
-	}
-	return;
+	cout << sizeof(arr);
 }
 
-void solver(std::multiset<int>& villain, std::multiset<int>& hero, size_t& size)
-{
-	for (int i{ 0 }; i < size; ++i)
-	{
-		auto itrVillain = villain.begin();
-		auto itrHero = hero.begin();
-		std::advance(itrHero, i);
-		std::advance(itrVillain, i);
-		if (*itrHero < *itrVillain) {
-			std::cout << "LOSE";
-			return;
-		}
-	}
 
-	std::cout << "WIN";
-}
 int main()
 {
-	int nOfTestCases{};
-	std::cin >> nOfTestCases;
-	for (int i{ 0 }; i < nOfTestCases; ++i)
-	{
-		using namespace std;
-		size_t size;
-		string tempInput{};
-		multiset<int> villain{};
-		multiset<int> hero{};
-		cin >> size;
-		cin.ignore();
-		getline(cin, tempInput);
-		filler(villain, tempInput);
-		getline(cin, tempInput);
-		filler(hero, tempInput);
-		solver(villain, hero, size);
-	}
-
+	int arr[]{ 1,2,3 };
+	SIZE<int,3>(arr);
 	return 0;
-	
 }
