1857735650844e01a68b9903ccf241a3a56a1e40
Arvind Sagar <48724777+cryoelite@users.noreply.github.com>
2020-09-11 23:50:14 +0530

    Removed Orientation

--- Progress/Flutter/augment_zero/lib/FirstRoute/Route_CameraView.dart
+++ Progress/Flutter/augment_zero/lib/FirstRoute/Route_CameraView.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:arcore_flutter_plugin/arcore_flutter_plugin.dart';
 import 'package:vector_math/vector_math_64.dart' as vector;
-import 'package:flutter/services.dart';
-
 import 'package:augment_zero/Utility/Class_OrientationObserver.dart';
 
 enum CAMSTATE {
@@ -11,8 +9,12 @@ enum CAMSTATE {
 }
 
 class RouteCameraView extends StatefulWidget {
+  final String materialColor;
+  final String shape;
   final Stream<CAMSTATE> cameraButtonStream;
-  RouteCameraView(this.cameraButtonStream, {Key key}) : super(key: key);
+  RouteCameraView(this.cameraButtonStream, this.materialColor, this.shape,
+      {Key key})
+      : super(key: key);
   @override
   _RouteCameraViewState createState() => _RouteCameraViewState();
 }
@@ -42,43 +44,16 @@ class _RouteCameraViewState extends State<RouteCameraView>
     });
   }
 
-  Widget build(BuildContext context) {
-    return StreamBuilder<ORIENTATION>(
-      stream: accelStreamClr.stream,
-      builder: (context, AsyncSnapshot<ORIENTATION> snapshot) {
-        if (snapshot.data == ORIENTATION.PORTRAIT) {
-          SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
-          setConstraints();
+  void dispose() {
+    arCoreController.dispose();
 
-          return buildPortrait();
-        } else {
-          SystemChrome.setPreferredOrientations(
-              [DeviceOrientation.landscapeLeft]);
-          setConstraints();
-
-          return buildLandScape();
-        }
-      },
-    );
+    super.dispose();
   }
 
-  Widget buildPortrait() {
+  Widget build(BuildContext context) {
     return Container(
       width: maxWidth,
       height: maxHeight - (minHeight * 23),
-      child: ArCoreView(
-        onArCoreViewCreated: onArCoreViewCreated,
-      ),
-    );
-  }
-
-  Widget buildLandScape() {
-    return Container(
-      width: maxHeight,
-      height: maxWidth - (minWidth * 23),
-      child: ArCoreView(
-        onArCoreViewCreated: onArCoreViewCreated,
-      ),
     );
   }
 }
--- Progress/Flutter/augment_zero/lib/FirstRoute/Route_Home.dart
+++ Progress/Flutter/augment_zero/lib/FirstRoute/Route_Home.dart
@@ -1,11 +1,17 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:torch_compat/torch_compat.dart';
 
 import 'package:augment_zero/Utility/Class_ScreenConf.dart';
 import 'package:augment_zero/FirstRoute/Route_CameraView.dart';
 import 'package:augment_zero/Utility/Class_OrientationObserver.dart';
 import 'package:flutter/services.dart';
+
+enum FLASHSTATUS {
+  ON,
+  OFF,
+}
 
 class RouteHome extends StatefulWidget {
   final ClassScreenConf getClass;
@@ -18,10 +24,93 @@ class RouteHome extends StatefulWidget {
 }
 
 class _RouteHomeState extends State<RouteHome> with ClassOrientationObserver {
+  String shape = "Cube";
+  String materialColor = "Red";
   StreamController<CAMSTATE> cameraButton = StreamController<CAMSTATE>();
-
+  FLASHSTATUS flashstatus = FLASHSTATUS.OFF;
+  List<DropdownMenuItem<dynamic>> menuItems = [];
+  List<DropdownMenuItem<dynamic>> shapeItems = [];
+  List<DropdownMenuItem<dynamic>> materialColorItems = [];
   _RouteHomeState() {
     setConstraints();
+    menuItems = [
+      DropdownMenuItem<DropdownButton>(
+        child: DropdownButton<dynamic>(
+          items: shapeItems,
+          hint: Text("Shape"),
+          onChanged: (value) {
+            shape = value.toString();
+          },
+          value: shape,
+        ),
+        onTap: () {},
+      ),
+      DropdownMenuItem(
+        child: Text("Color"),
+        onTap: () {},
+      ),
+    ];
+
+    shapeItems = [
+      DropdownMenuItem(
+        child: Text("Cube"),
+        onTap: () {
+          shape = "Cube";
+        },
+        value: shape,
+      ),
+      DropdownMenuItem(
+        child: Text("Sphere"),
+        onTap: () {
+          shape = "Sphere";
+        },
+        value: shape,
+      ),
+      DropdownMenuItem(
+        child: Text("Cylinder"),
+        onTap: () {
+          shape = "Cylinder";
+        },
+        value: shape,
+      ),
+    ];
+
+    materialColorItems = [
+      DropdownMenuItem(
+        child: Text("Blue"),
+        onTap: () {
+          materialColor = "Blue";
+        },
+        value: materialColor,
+      ),
+    ];
+    materialColorItems = [
+      DropdownMenuItem(
+        child: Text("Red"),
+        onTap: () {
+          materialColor = "Red";
+        },
+        value: materialColor,
+      ),
+    ];
+    materialColorItems = [
+      DropdownMenuItem(
+        child: Text("Yellow"),
+        onTap: () {
+          materialColor = "Yellow";
+        },
+        value: materialColor,
+      ),
+    ];
+    materialColorItems = [
+      DropdownMenuItem(
+        child: Text("Green"),
+        onTap: () {
+          materialColor = "Green";
+        },
+        value: materialColor,
+      ),
+    ];
   }
 
   @override
@@ -39,146 +128,222 @@ class _RouteHomeState extends State<RouteHome> with ClassOrientationObserver {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
     return Scaffold(
       backgroundColor: Colors.black,
-      body: StreamBuilder<ORIENTATION>(
-        stream: accelStreamClr.stream,
-        builder: (context, AsyncSnapshot<ORIENTATION> snapshot) {
-          if (snapshot.data == ORIENTATION.PORTRAIT) {
-            SystemChrome.setPreferredOrientations(
-                [DeviceOrientation.portraitUp]);
-            setConstraints();
+      body: Container(
+        width: maxWidth,
+        height: maxHeight,
+        child: Stack(
+          children: [
+            buildPortrait(),
+            /* StreamBuilder<ORIENTATION>(
+              stream: accelStreamClr.stream,
+              builder: (context, AsyncSnapshot<ORIENTATION> snapshot) {
+                setConstraints();
 
-            return buildPortrait();
-          } else {
-            SystemChrome.setPreferredOrientations(
-                [DeviceOrientation.landscapeLeft]);
-            setConstraints();
+                if (snapshot.data == ORIENTATION.PORTRAIT) {
+                  SystemChrome.setPreferredOrientations(
+                      [DeviceOrientation.portraitUp]);
+                  return 
+                } else {
+                  SystemChrome.setPreferredOrientations(
+                      [DeviceOrientation.landscapeLeft]);
 
-            return buildLandScape();
-          }
-        },
+                  return buildLandScape();
+                }
+              },
+            ), */
+            Positioned(
+              top: minHeight * 8,
+              child: buildcameraWindow(),
+            ),
+          ],
+        ),
       ),
     );
   }
 
-  Container buildLandScape() {
-    return Container(
-      width: maxWidth,
-      height: maxHeight,
-      child: Stack(
-        children: <Widget>[
-          Positioned(
-            left: minWidth * 1,
-            top: minHeight * 1,
-            child: Container(
-              width: minWidth * 8,
-              height: minHeight * 15,
-              color: Colors.black,
-              alignment: Alignment.center,
-              child: Container(
-                child: Material(
-                  color: Colors.transparent,
-                  child: IconButton(
-                    iconSize: minHeight * 9,
-                    icon: Icon(
-                      Icons.settings,
-                      color: Colors.white,
-                    ),
-                    onPressed: () {},
-                  ),
-                ),
-              ),
+  Widget buildcameraWindow() {
+    print(shapeItems.length);
+    return RouteCameraView(
+      cameraButton.stream,
+      materialColor,
+      shape,
+    );
+  }
+
+  Stack buildLandScape() {
+    return Stack(
+      children: [
+        buildSettingsButtonLandScape(),
+        buildCamButtonLandScape(),
+      ],
+    );
+  }
+
+  Stack buildPortrait() {
+    return Stack(
+      children: [
+        buildCamButtonPortrait(),
+        buildSettingsButtonPortrait(),
+        buildFlashLightTogglePortrait()
+      ],
+    );
+  }
+
+  Positioned buildCamButtonLandScape() {
+    return Positioned(
+      right: 1,
+      child: Container(
+        width: minWidth * 15,
+        height: maxHeight,
+        color: Colors.amber,
+        alignment: Alignment.center,
+        child: Container(
+          child: IconButton(
+            iconSize: minWidth * 10,
+            padding: EdgeInsets.all(0),
+            icon: Icon(
+              Icons.ac_unit,
+              color: Colors.white,
             ),
+            onPressed: () {
+              cameraButton.add(CAMSTATE.PRESSED);
+              print("Cam pressed!");
+            },
           ),
-          Positioned(
-            left: minWidth * 8,
-            child: RouteCameraView(cameraButton.stream),
-          ),
-          Positioned(
-            right: 1,
-            child: Container(
-              width: minWidth * 15,
-              height: maxHeight,
-              alignment: Alignment.center,
-              child: Container(
-                child: IconButton(
-                  iconSize: minWidth * 10,
-                  padding: EdgeInsets.all(0),
-                  icon: Icon(
-                    Icons.ac_unit,
-                    color: Colors.white,
-                  ),
-                  onPressed: () {
-                    cameraButton.add(CAMSTATE.PRESSED);
-                    print("Cam pressed!");
-                  },
-                ),
-              ),
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
 
-  Container buildPortrait() {
-    return Container(
-      width: maxWidth,
-      height: maxHeight,
-      child: Stack(
-        children: <Widget>[
-          Positioned(
-            left: minWidth * 1,
-            top: minHeight * 0.5,
-            child: Container(
-              width: minWidth * 15,
-              height: minHeight * 8,
-              color: Colors.black,
-              alignment: Alignment.center,
-              child: Container(
-                child: Material(
-                  color: Colors.transparent,
-                  child: IconButton(
-                    iconSize: minWidth * 9,
-                    icon: Icon(
-                      Icons.settings,
-                      color: Colors.white,
-                    ),
-                    onPressed: () {},
-                  ),
-                ),
+  Positioned buildSettingsButtonLandScape() {
+    return Positioned(
+      left: minWidth * 1,
+      top: minHeight * 1,
+      child: Container(
+        width: minWidth * 8,
+        height: minHeight * 15,
+        color: Colors.black,
+        alignment: Alignment.center,
+        child: Container(
+          child: Material(
+            color: Colors.transparent,
+            child: IconButton(
+              iconSize: minHeight * 9,
+              icon: Icon(
+                Icons.settings,
+                color: Colors.white,
               ),
+              onPressed: () {},
             ),
           ),
-          Positioned(
-            top: minHeight * 8,
-            child: RouteCameraView(cameraButton.stream),
-          ),
-          Positioned(
-            bottom: 0,
-            child: Container(
-              width: maxWidth,
-              height: minHeight * 15,
-              alignment: Alignment.center,
-              child: Container(
-                child: IconButton(
-                  iconSize: minHeight * 10,
-                  padding: EdgeInsets.all(0),
-                  icon: Icon(
-                    Icons.ac_unit,
-                    color: Colors.white,
-                  ),
-                  onPressed: () {
-                    cameraButton.add(CAMSTATE.PRESSED);
-                    print("Cam pressed!");
-                  },
-                ),
-              ),
-            ),
-          ),
-        ],
+        ),
       ),
     );
+  }
+
+  Positioned buildCamButtonPortrait() {
+    return Positioned(
+      bottom: 0,
+      child: Container(
+        width: maxWidth,
+        height: minHeight * 15,
+        alignment: Alignment.center,
+        color: Colors.black,
+        child: Container(
+          child: IconButton(
+            iconSize: minHeight * 10,
+            padding: EdgeInsets.all(0),
+            icon: Icon(
+              Icons.ac_unit,
+              color: Colors.white,
+            ),
+            onPressed: () {
+              cameraButton.add(CAMSTATE.PRESSED);
+              print("Cam pressed!");
+            },
+          ),
+        ),
+      ),
+    );
+  }
+
+  Positioned buildSettingsButtonPortrait() {
+    return Positioned(
+      left: minWidth,
+      top: minHeight * 0.5,
+      child: Container(
+        color: Colors.black,
+        alignment: Alignment.center,
+        child: Container(
+          child: Material(
+            color: Colors.transparent,
+            child: DropdownButton(
+              items: menuItems,
+              icon: Icon(
+                Icons.settings,
+                color: Colors.white,
+              ),
+              iconSize: minWidth * 9,
+              onTap: () {},
+              onChanged: (_) {},
+              elevation: 0,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Positioned buildFlashLightTogglePortrait() {
+    return Positioned(
+      right: minWidth * 1,
+      top: minHeight * 0.5,
+      child: Container(
+        width: minWidth * 15,
+        height: minHeight * 8,
+        color: Colors.black,
+        alignment: Alignment.center,
+        child: Container(
+          child: Material(
+            color: Colors.transparent,
+            child: IconButton(
+              icon: buildFlashIcon(),
+              iconSize: minWidth * 9,
+              onPressed: flashToggle,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Icon buildFlashIcon() {
+    if (flashstatus == FLASHSTATUS.OFF) {
+      return Icon(
+        Icons.flash_off,
+        color: Colors.white,
+      );
+    } else {
+      return Icon(
+        Icons.flash_on,
+        color: Colors.white,
+      );
+    }
+  }
+
+  void flashToggle() async {
+    if (flashstatus == FLASHSTATUS.OFF) {
+      await TorchCompat.turnOn();
+      flashstatus = FLASHSTATUS.ON;
+      setState(() {});
+    } else {
+      await TorchCompat.turnOff();
+
+      flashstatus = FLASHSTATUS.OFF;
+      setState(() {});
+    }
   }
 }
